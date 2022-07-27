2f5ae97fcadc84cbbd368d1881cbf4f5120b9801
Nsovo Ngobeni <97952201+MrSlovo@users.noreply.github.com>
2022-07-28 00:23:08 +0200

    Added date time to scramble obj
    Added a date time object and corresponding getters and setters for all fields withing the class

--- src/Scramble.java
+++ src/Scramble.java
@@ -1,9 +1,13 @@
+import java.time.LocalDateTime;
+
 /** A Scramble object that stores the relevent data relating to a scramble */
 public class Scramble {
 	/** The scramble written in standard cube notation */
-	private String scrammble;
+	private String scramble;
 	/** The time taken to solve the scramble (in milliseconds) */
 	private int time;
+	/** The date and time the solve was completed */
+	private LocalDateTime timeOfSolve;
 
 	/**
 	 * Instantiates a new Scramble object
@@ -14,8 +18,9 @@ public class Scramble {
 	 *                  solve the scramble in milliseconds
 	 */
 	public Scramble(String scrammble, int time) {
-		this.scrammble = scrammble;
+		this.scramble = scrammble;
 		this.time = time;
+		timeOfSolve = LocalDateTime.now();
 	}
 
 	/**
@@ -24,7 +29,16 @@ public class Scramble {
 	 * @return A string containing the scramble used
 	 */
 	public String getScramble() {
-		return scrammble;
+		return scramble;
+	}
+
+	/**
+	 * Changes the scramble
+	 * 
+	 * @param scramble The new scramble to be stored
+	 */
+	public void setScramble(String scramble) {
+		this.scramble = scramble;
 	}
 
 	/**
@@ -34,5 +48,40 @@ public class Scramble {
 	 */
 	public int getTime() {
 		return time;
+	}
+
+	/**
+	 * Changes the time taken to solve the scramble
+	 * 
+	 * @param time An integer value with the new time value in milliseconds
+	 */
+	public void setTime(int time) {
+		this.time = time;
+	}
+
+	/**
+	 * Returns the time the solve was completed
+	 * 
+	 * @return A LocalDateTime object containg the date the solve was completed
+	 */
+	public LocalDateTime getTimeOfSolve() {
+		return timeOfSolve;
+	}
+
+	/**
+	 * Changes the date and time the scramble was completed to the current date and
+	 * time
+	 */
+	public void setTimeOfSolve() {
+		this.timeOfSolve = LocalDateTime.now();
+	}
+
+	/**
+	 * Changes the date and time the scramble was completed
+	 * 
+	 * @param timeOfSolve The new time of solve
+	 */
+	public void setTimeOfSolve(LocalDateTime timeOfSolve) {
+		this.timeOfSolve = timeOfSolve;
 	}
 }
